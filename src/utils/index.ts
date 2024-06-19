118036c727445a6e7e04dcc1de54432587cdba76
Roger BC <roger@chaindots.com>
2024-06-19 14:00:28 -0300

    add: IsEmpty are added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import { StockData } from '@/types';
 const API_KEY = import.meta.env.VITE_API_KEY;
 export function fetchGET(url: string) {
   return fetch(url, {
@@ -9,3 +10,11 @@ export const headers: HeadersInit = {
   'content-type': 'application/json',
   Authorization: `apikey ${API_KEY}`,
 };
+
+export function isEmptyObject(obj: {}) {
+  return Object.keys(obj).length === 0;
+}
+export function isEmptyArray(array: StockData[]) {
+  return array.length === 0;
+}
+
