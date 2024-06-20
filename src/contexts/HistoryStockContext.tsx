431fe963642e770e1e978e946c3a88d8583a3976
Roger BC <roger@chaindots.com>
2024-06-20 00:04:38 -0300

    add: refForm

--- src/contexts/HistoryStockContext.tsx
+++ src/contexts/HistoryStockContext.tsx
@@ -1,6 +1,6 @@
 import { useFetchHistory } from '@/hooks/useFetchHistory';
 import { HistoryStockContextProps } from '@/types';
-import { PropsWithChildren, createContext, useContext } from 'react';
+import { PropsWithChildren, createContext, useContext, useRef } from 'react';
 
 const HistoryStockContext = createContext<HistoryStockContextProps>(
   {} as HistoryStockContextProps
@@ -11,10 +11,18 @@ export const useHistoryStock = () => useContext(HistoryStockContext);
 export const HistoryStockProvider = ({ children }: PropsWithChildren) => {
   const { loading, setGetHistoryProps, meta, values, error } =
     useFetchHistory();
+  const refForm = useRef<HTMLFormElement | null>(null);
 
   return (
     <HistoryStockContext.Provider
-      value={{ loading, meta, values, error, setGetHistoryProps }}
+      value={{
+        loading,
+        meta,
+        values,
+        error,
+        refForm,
+        setGetHistoryProps,
+      }}
     >
       {children}
     </HistoryStockContext.Provider>
